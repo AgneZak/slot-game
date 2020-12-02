07103e3f559acab2a0c8f899bb8d37dce3ba9923
Engaz01 <azakaite@github.com>
2020-12-02 17:05:34 +0200

    Add changed tables

--- public_html/install.php
+++ public_html/install.php
@@ -10,4 +10,4 @@ App::$db = new FileDB(DB_FILE);
 App::$db->createTable('users');
 App::$db->insertRow('users', ['username' => 'test1', 'password' => 'test', 'gems' => 0]);
 
-App::$db->createTable('items');
+App::$db->createTable('history');
